91c7a6a4aa17e9de570139f74180a30e13d0c9c8
AnshRehan04 <anshrehan7@gmail.com>
2023-06-19 13:34:50 +0530

    conditonals

---
+++ Day1/Conditionals and Loops/1.cpp
@@ -0,0 +1,14 @@
+// Check wheather a is positive or negative
+
+#include <iostream>
+using namespace std;
+int main(){
+    int a;
+    cin>>a;
+    if(a>0){
+        cout<<"A is Positive"<<endl;
+    }
+    else{
+        cout<<"A is Negative"<<endl;
+    }
+}
