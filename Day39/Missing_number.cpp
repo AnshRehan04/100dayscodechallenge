bbb1459d2d0e1ecf0678a4cdb58718583bd14f08
AnshRehan04 <anshrehan7@gmail.com>
2023-07-29 19:45:36 +0530

    Day39

---
+++ Day39/Missing_number.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        int s=0;
+        int e=nums.size()-1;
+        while(s<=e){
+            int mid=s+(e-s)/2;
+            if(nums[mid]==mid){
+                s=mid+1;
+            }
+            else{
+                e=mid-1;
+            }
+        }
+        return s;
+    }
+};
