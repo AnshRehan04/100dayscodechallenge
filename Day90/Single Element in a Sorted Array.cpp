ccf006e05bd0b0509a063e8984e5c92516e3ef45
Ansh <anshrehan04@gmail.com>
2023-09-18 21:48:54 +0530

    Day90

---
+++ Day90/Single Element in a Sorted Array.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int singleNonDuplicate(vector<int>& nums) {
+        int s=0;
+        int e=nums.size()-2;
+       
+        while(s<=e){
+             int mid=(s+e)/2;
+            if(nums[mid]==nums[mid^1]){
+                s=mid+1;
+            }
+            else{
+                e=mid-1;
+            }
+        }
+        return nums[s];
+    }
+};
