78067e5b3169f5b525adcadab0442eae3b091dbe
Ansh <anshrehan04@gmail.com>
2023-09-20 22:08:35 +0530

    Day92

---
+++ Day92/Find All Numbers Disappeared in an Array.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<int> findDisappearedNumbers(vector<int>& nums) {
+        int n=nums.size();
+        vector<int> ans;
+       
+        for(int i=0;i<n;i++){
+             int mark=abs(nums[i])-1;
+            if(nums[mark]>0){
+                nums[mark]*=-1;
+            }
+        }
+        for(int i=0;i<n;i++){
+            if(nums[i]>0){
+                ans.push_back(i+1);
+            }
+        }
+        return ans;
+    }
+};
